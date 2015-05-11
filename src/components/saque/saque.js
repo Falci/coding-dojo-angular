7e59c17ab22ae3032d4cad16a24b481e3b6f8dfe
luigugah <luigugah@gmail.com>
2015-05-11 19:48:56 -0300

    Implementado dojo de saque

--- src/components/saque/saque.js
+++ src/components/saque/saque.js
@@ -4,13 +4,33 @@ angular.module('app.components.Saque', [])
 function SaqueProvider () {
   this.$get = Saque;
 
-  function Saque (){
+  function Saque (){    
+
     return {
       calcular: calcular
     };
 
     function calcular (valor) {
-      return [valor];
+      var notas = [{valor: 100,quantidade: 2}, {valor: 50,quantidade: 2},
+                   {valor: 20,quantidade: 2}, {valor: 10,quantidade: 2}];
+
+      console.log(typeof valor, valor)
+
+      var retorno = [];
+
+      if (isNaN(valor)) {return false;}
+
+      notas.forEach(function (nota){
+        while(nota.valor <= valor && nota.quantidade){
+          retorno.push(nota.valor);
+          valor -= nota.valor;
+          nota.quantidade--;
+        }
+      })
+
+      if(valor > 0){return false;}
+
+      return retorno;
     }
   }
 };
